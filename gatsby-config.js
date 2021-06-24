c984a1f93788564b47f95a96f63bab21159b0846
Donny Wilson <heydwilson@gmail.com>
2021-06-23 21:34:49 -0400

    Initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ipsum`,
+    description: `Space Site For Learning and Stuff`,
+    author: `Donny Wilson`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,13 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from '../images/ipsum_logo.svg'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <img src={logo} alt="Ipsum Logo" />
+    <div>
+      {siteTitle}
     </div>
   </header>
 )
@@ -36,7 +17,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `Ipsum`,
 }
 
 export default Header
